6edffd8b2f8cc6afe15add20347c92cd022bdb75
wct8tc <wct8tc@rc05xcs213.managed.mst.edu>
2017-10-25 16:16:27 -0500

    Adding utility files

--- run.sh
+++ run.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-echo
-bison -d sql_parser.y
-if [ $? -ne 0 ]; then
-  echo "Bison Failed! Stopping..."
-  exit 1
-fi
-
-flex sql_parser.l
-if [ $? -ne 0 ]; then
-  echo "Flex Failed! Stopping..."
-  exit 1
-fi
-
-g++ sql_parser.tab.c lex.yy.c -o sqlComp
-if [ $? -ne 0 ]; then
-  echo "Compiling Failed! Stopping..."
-  exit 1
-fi
-
 echo "Enter the query, CTRL-D when done"
 
 > test.txt
@@ -30,3 +11,4 @@ cat test.txt
 echo
 echo "Results"
 sqlComp < test.txt
+
